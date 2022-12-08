33d4c2e182a4d3fd05862a61945d2e1a998e60c0
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-08 17:40:06 +0100

    error

--- js/dom.js
+++ js/dom.js
@@ -6,17 +6,30 @@
 // 	- createAttribute()
 // 	- createTextNode()
 
+// Create a "li" element:
+const newNode1 = document.createElement("li");
+const newNode2 = document.createElement("li");
 
-const contenedor = document.getElementById('milista');
+// Create a text node:
+const fruta1 = document.createTextNode("Guanabana");
+const fruta2 = document.createTextNode("Pera");
+// Append text node to "li" element:
+newNode1.appendChild(fruta1);
+newNode2.appendChild(fruta2);
+
+// Insert before existing child:
+const list = document.getElementById("milista");
+list.insertBefore(newNode1, list.children[0]);
+list.insertBefore(newNode2, list.children[1]);
+
+
+/* const contenedor = document.getElementById('milista').innerHTML;
 const lista = document.createElement('ul');
 
 // creo los li 
 const fruta1 =document.createElement('li');
 const fruta2 =document.createElement('li');
 
-//relaciono los li con el ul
-lista.appendChild(fruta1);
-lista.appendChild(fruta2);
 
 //creo los textos para las li
 const textoFruta1 = document.createTextNode('guanabana');
@@ -30,19 +43,15 @@ console.log(fruta1);
 console.log(fruta2);
 console.log(lista);
 
-
-
-//asigno cada li al texto de la ul
-// lista.insertAdjacentElement('beforebegin',fruta1);
-
-/* lista.appendChild(fruta1);
+//relaciono los li con el ul
+lista.appendChild(fruta1);
 lista.appendChild(fruta2);
- */
 
-/* 
+
+
 lista.insertBefore(fruta1, lista.children[0]);
-lista.insertBefore(fruta2, lista.children[1]); */
-
+lista.insertBefore(fruta2, lista.children[1]); 
+ */
 // Hacer un boton que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por defecto, el párrafo debe estar visible. Dar estilos a los elementos afectados
 /*  */
 // Hacer un boton que esconda/enseñe el texto de un parrafo cada vez que se pulsa el mismo boton. Por
